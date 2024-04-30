bca55851657764dc63224dbe32251536c0ca250e
Brad Campbell <bradjc5@gmail.com>
2024-04-29 22:52:36 -0400

    test: i2c

--- examples/tests/i2c/i2c_master_slave_ping_pong/main.c
+++ examples/tests/i2c/i2c_master_slave_ping_pong/main.c
@@ -4,8 +4,8 @@
 #include <string.h>
 
 #include <libtock/interface/button.h>
-#include <libtock/peripherals/i2c_master.h>
-#include <libtock/timer.h>libtock_
+#include <libtock/peripherals/i2c_master_slave.h>
+#include <libtock/timer.h>
 
 #define BUF_SIZE 16
 #define LEADER_ADDRESS 0x40
@@ -32,7 +32,7 @@ static void i2c_callback(int                            callback_type,
     libtock_button_count(&nbuttons);
     int j;
     for (j = 0; j < nbuttons; j++) {
-      libtock_button_disable_interrupt(j);
+      libtock_button_command_disable_interrupt(j);
     }
   }
 
@@ -54,10 +54,10 @@ static void i2c_callback(int                            callback_type,
 // A button press indicates that this device should start the ping-pong
 // exchange. First, change the address to the BUTTON_ADDRESS to avoid
 // conflict with the other node, then send a message.
-static void button_cb(__attribute__((unused)) int    btn_num,
-                      __attribute__ ((unused)) int   arg1,
-                      __attribute__ ((unused)) int   arg2,
-                      __attribute__ ((unused)) void* userdata) {
+static void button_cb(
+  __attribute__((unused)) returncode_t    ret,
+  __attribute__((unused)) int    btn_num,
+                      __attribute__ ((unused)) bool   val) {
   // Only the first press is meaningfull
   static bool pressed = false;
 
@@ -92,7 +92,7 @@ int main(void) {
   TOCK_EXPECT(RETURNCODE_SUCCESS, i2c_master_slave_listen());
 
   int nbuttons;
-  button_count(&nbuttons);
+  libtock_button_count(&nbuttons);
   if (nbuttons < 1) {
     printf("ERROR: This app requires that a board have at least one button.\n");
     exit(-1);
