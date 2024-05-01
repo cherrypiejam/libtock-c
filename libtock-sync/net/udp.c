49a322f2b2f7aea84f812d6642de81e7afa562a0
Tyler Potyondy <potyondyt@gmail.com>
2024-05-01 16:08:26 -0700

    udp

--- libtock-sync/net/udp.c
+++ libtock-sync/net/udp.c
@@ -5,30 +5,30 @@
 struct recv_data {
   bool fired;
   int val;
-  returncode_t result;
+  statuscode_t status;
 };
 
 struct send_data {
   bool fired;
-  returncode_t result;
+  statuscode_t status;
 };
 
 static struct send_data send_sync_result = { .fired = false };
 static struct recv_data recv_sync_result = { .fired = false };
 
-static void send_callback(returncode_t ret) {
+static void send_callback(statuscode_t ret) {
   send_sync_result.fired  = true;
-  send_sync_result.result = ret;
+  send_sync_result.status = ret;
 }
 
-static void recv_callback(returncode_t ret, int len) {
+static void recv_callback(statuscode_t ret, int len) {
   recv_sync_result.val    = len;
   recv_sync_result.fired  = true;
-  recv_sync_result.result = ret;
+  recv_sync_result.status = ret;
 }
 
-returncode_t libtock_udp_send_sync(void *buf, size_t len,
-                                   sock_addr_t *dst_addr) {
+returncode_t libtocksync_udp_send(void *buf, size_t len,
+                                  sock_addr_t *dst_addr) {
   returncode_t ret;
   send_sync_result.fired = false;
 
@@ -36,10 +36,10 @@ returncode_t libtock_udp_send_sync(void *buf, size_t len,
   if (ret != RETURNCODE_SUCCESS) return ret;
 
   yield_for(&send_sync_result.fired);
-  return send_sync_result.result;
+  return tock_status_to_returncode(send_sync_result.status);
 }
 
-returncode_t libtock_udp_recv_sync(void *buf, size_t len, size_t* received_len) {
+returncode_t libtocksync_udp_recv(void *buf, size_t len, size_t* received_len) {
   returncode_t ret;
   recv_sync_result.fired = false;
 
@@ -49,5 +49,5 @@ returncode_t libtock_udp_recv_sync(void *buf, size_t len, size_t* received_len) 
   yield_for(&recv_sync_result.fired);
 
   *received_len = recv_sync_result.val;
-  return recv_sync_result.result;
+  return tock_status_to_returncode(recv_sync_result.status);
 }
--- libtock-sync/net/udp.h
+++ libtock-sync/net/udp.h
@@ -9,10 +9,10 @@ extern "C" {
 
 // TODO: fix these names
 
-returncode_t libtock_udp_send_sync(void *buf, size_t len,
+returncode_t libtocksync_udp_send(void *buf, size_t len,
                     sock_addr_t *dst_addr);
 
-returncode_t libtock_udp_recv_sync(void *buf, size_t len, size_t* received_len);
+returncode_t libtocksync_udp_recv(void *buf, size_t len, size_t* received_len);
 
 #ifdef __cplusplus
 }
