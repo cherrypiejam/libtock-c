db174894292237a8f65cfb3d20a3b8175c62625d
Brad Campbell <bradjc5@gmail.com>
2024-05-02 09:34:36 -0700

    tutorial: 05 ipc

--- examples/tutorials/05_ipc/logic/main.c
+++ examples/tutorials/05_ipc/logic/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <kernel/ipc.h>
-#include <timer.h>
+#include <libtock/kernel/ipc.h>
+#include <libtock/timer.h>
 
 // Every 500 ms use the RNG service to randomly select an LED to turn on or
 // off and then use the LED service to control that LED.
